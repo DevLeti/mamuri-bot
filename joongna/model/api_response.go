598223dddb78c51aecf119ab41fdf5449b103125
phobyjun <phobyjun@gmail.com>
2022-05-11 00:28:12 +0900

    ADD: api response model for processing naver cafe searching api response

---
+++ joongna/model/api_response.go
@@ -0,0 +1,17 @@
+package model
+
+type ApiResponse struct {
+	LastBuildDate string            `json:"lastBuildDate"`
+	Total         uint              `json:"total"`
+	Start         uint              `json:"start"`
+	Display       uint              `json:"display"`
+	Items         []ApiResponseItem `json:"items"`
+}
+
+type ApiResponseItem struct {
+	Title       string `json:"title"`
+	Link        string `json:"link"`
+	Description string `json:"description"`
+	CafeName    string `json:"cafename"`
+	CafeUrl     string `json:"cafeurl"`
+}
