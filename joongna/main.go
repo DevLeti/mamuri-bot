7b7cb3f8103ef351b2ce1571329c6944d366d8ff
phobyjun <phobyjun@gmail.com>
2022-05-10 23:42:56 +0900

    ADD: install echo framework for joongna api and init main.go

--- joongna/main.go
+++ joongna/main.go
@@ -1,34 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"joongna/config"
-	"log"
-	"net/http"
-	url2 "net/url"
-)
+import "github.com/labstack/echo/v4"
 
 func main() {
-	keyword := "m1 pro 맥북 프로 16인치"
-	encText := url2.QueryEscape("중고나라" + keyword)
-	url := "https://openapi.naver.com/v1/search/cafearticle.json?query=" + encText + "&sort=sim"
+	//keyword := "m1 pro 맥북 프로 16인치"
+	//encText := url2.QueryEscape("중고나라" + keyword)
+	//url := "https://openapi.naver.com/v1/search/cafearticle.json?query=" + encText + "&sort=sim"
+	//
+	//req, err := http.NewRequest("GET", url, nil)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//req.Header.Add("X-Naver-Client-Id", config.Cfg.Secret.CLIENTID)
+	//req.Header.Add("X-Naver-Client-Secret", config.Cfg.Secret.CLIENTSECRET)
+	//
+	//client := &http.Client{}
+	//resp, err := client.Do(req)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer resp.Body.Close()
+	//
+	//bytes, _ := ioutil.ReadAll(resp.Body)
+	//str := string(bytes)
+	//fmt.Println(str)
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Add("X-Naver-Client-Id", config.Cfg.Secret.CLIENTID)
-	req.Header.Add("X-Naver-Client-Secret", config.Cfg.Secret.CLIENTSECRET)
+	e := echo.New()
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	str := string(bytes)
-	fmt.Println(str)
+	e.Logger.Fatal(e.Start(":8080"))
 }
