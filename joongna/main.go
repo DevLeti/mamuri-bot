dc6daa0a2c9335362e3f54d98a4a0fd1b72e6af2
phobyjun <phobyjun@gmail.com>
2022-05-13 23:46:28 +0900

    ADD: router initialization in main function

--- joongna/main.go
+++ joongna/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"joongna/router"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
 	e := echo.New()
+
+	router.Init(e)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
