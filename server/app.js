541412fa8fe1fb0bc58227f3f47603cb8d434ef6
유명현 <mhlew0106@khu.ac.kr>
2022-05-14 19:19:29 +0900

    Set line-bot sdk, .env

--- server/app.js
+++ server/app.js
@@ -1,8 +1,35 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const line = require("@line/bot-sdk");
+require("dotenv").config();
+console.log(process.env.channelAccessToken, process.env.channelSecret);
+const config = {
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret,
+};
 
-app.get("/", function (req, res) {
-  res.send("<h1>hello express!</h1>");
+const app = express();
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
 });
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+  console.log(event);
+
+  // push
+  client.pushMessage(event.source.userId, {
+    type: "text",
+    text: "hihihi",
+  });
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text,
+  });
+}
 
 app.listen(3000);
